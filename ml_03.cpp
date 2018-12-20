4b0978b3ec2c3decead20f24dba1a929a095605c
Petr Pogorelov <peter.pogorelov@gmail.com>
2018-12-20 19:21:33 +0300

    Add files via upload

--- ml_03.cpp
+++ ml_03.cpp
@@ -6,30 +6,31 @@
 #include <algorithm>
 #include <numeric>
 #include <limits>
+#include <deque>
 
 struct similarity_record {
 	size_t from, to;
 	double value;
 };
 
-similarity_record extract_ids(std::string line, bool has_similarity=false)
+similarity_record extract_ids(std::string line, bool has_similarity = false)
 {
-	similarity_record record{0, 0, 1.};
+	similarity_record record{ 0, 0, 1. };
 
 	std::stringstream ss;
 	ss << line;
 
-ss >> line;
-std::stringstream(line) >> record.from;
-ss >> line;
-std::stringstream(line) >> record.to;
-
-if (has_similarity) {
 	ss >> line;
-	std::stringstream(line) >> record.value;
-}
+	std::stringstream(line) >> record.from;
+	ss >> line;
+	std::stringstream(line) >> record.to;
 
-return record;
+	if (has_similarity) {
+		ss >> line;
+		std::stringstream(line) >> record.value;
+	}
+
+	return record;
 }
 
 void cache_exemplars(const char* file_name, const std::vector<size_t>& data) {
@@ -62,7 +63,7 @@ void cache_top_results(const char* file_name, const std::vector<std::vector<int>
 
 struct CEdgeAP {
 	int from, to;
-	float s, a, r;
+	double s, a, r;
 
 	CEdgeAP(int from, int to) :
 		from(from), to(to) {
@@ -78,24 +79,30 @@ class CGraphAP {
 	EdgeVector** pp_inputEdges = nullptr;
 	EdgeVector** pp_outputEdges = nullptr;
 	std::vector<size_t> exemplars;
+	std::vector<size_t> outputNodesCounts;
 	size_t i_matrixSize;
 
 	std::mt19937 generator;
-	std::normal_distribution<float> norm_distr;
+	std::normal_distribution<double> norm_distr;
 private:
-	CEdgeAP* make_edge(int from, int to, float s = -1) {
+	CEdgeAP* make_edge(int from, int to, double s = 1) {
 		CEdgeAP* edge = new CEdgeAP(from, to);
-		edge->s = s + this->next_random() * 1e-3f;
+		edge->s = s + this->next_random() * 1e-3;
 		return edge;
 	}
 
 	CEdgeAP* make_edge_pref(int node_id) {
 		CEdgeAP* edge = new CEdgeAP(node_id, node_id);
-		edge->s = this->next_random() * 1e-3f;
+		//edge->s = -2 + this->next_random() * 1e-2;
+
+		// смотреть вероятность от числа связей (корень от степени вершины)
+		//edge->s = 0.1;// + this->next_random() * 1e-3;
+
+		edge->s = std::log(this->outputNodesCounts[node_id] + 1) + this->next_random() * 1e-3;
 		return edge;
 	}
 
-	inline float next_random() {
+	inline double next_random() {
 		return this->norm_distr(this->generator);
 	}
 
@@ -112,7 +119,7 @@ private:
 
 public:
 	CGraphAP() {
-		this->norm_distr = std::normal_distribution<float>(0, 1.0);
+		this->norm_distr = std::normal_distribution<double>(0, 1.0);
 	}
 
 	~CGraphAP() {
@@ -130,14 +137,17 @@ public:
 		delete[] this->pp_outputEdges;
 	}
 
-	void read_from_file(const char* fname, size_t n_unique, bool has_similarity=false) {
+	void read_from_file(const char* fname, size_t n_unique, bool has_similarity = false) {
 		this->i_matrixSize = n_unique;
 
 		std::ifstream file(fname);
 		this->exemplars = std::vector<size_t>(n_unique, -1);
+		this->outputNodesCounts = std::vector<size_t>(n_unique, 0);
 
 		this->pp_inputEdges = new EdgeVector*[n_unique] {nullptr};
 		this->pp_outputEdges = new EdgeVector*[n_unique] {nullptr};
+
+		
 
 		if (file.is_open()) {
 			std::cout << "loading data..." << std::endl;
@@ -152,24 +162,25 @@ public:
 
 				similarity_record extracted = extract_ids(line, has_similarity);
 				this->append_record(this->make_edge(extracted.from, extracted.to, extracted.value));
+
+				this->outputNodesCounts[extracted.from]++;
 			}
 
 			// preferences
 			for (size_t i = 0; i < this->i_matrixSize; ++i) {
 				this->append_record(this->make_edge_pref(i));
 			}
-
-			file.close();
 		}
 	}
 };
 
 class CAffinityPropagation {
 	CGraphAP* p_graph;
-	float i_damping;
+	double i_damping;
 	size_t i_maxIter;
+	size_t i_convergenceIter;
 
-	inline void update_with_damping(float &_old, float _new) {
+	inline void update_with_damping(double &_old, double _new) {
 		_old = this->i_damping * _old + (1.f - this->i_damping) * (_new);
 	}
 
@@ -180,7 +191,7 @@ class CAffinityPropagation {
 		std::sort(exemplars_copy.begin(), exemplars_copy.end());
 		auto last = std::unique(exemplars_copy.begin(), exemplars_copy.end());
 		exemplars_copy.erase(last, exemplars_copy.end());
-		
+
 		return exemplars_copy.size();
 	}
 
@@ -188,10 +199,10 @@ class CAffinityPropagation {
 		for (size_t i = 0; i < p_graph->i_matrixSize; ++i) {
 			auto& p_edgesRow = this->p_graph->pp_outputEdges[i];
 
-			float first_maximum_value = -std::numeric_limits<float>::infinity();
-			float second_maximum_value = -std::numeric_limits<float>::infinity();
+			double first_maximum_value = -std::numeric_limits<double>::infinity();
+			double second_maximum_value = -std::numeric_limits<double>::infinity();
 			size_t maximum_index = -1;
-			float temp_value;
+			double temp_value;
 
 			// find fist and second maximums
 			for (size_t j = 0; j < p_edgesRow->size(); ++j) {
@@ -208,7 +219,7 @@ class CAffinityPropagation {
 			}
 
 			// update responsibilities
-			for (size_t j = 0; j < maximum_index; ++j){
+			for (size_t j = 0; j < maximum_index; ++j) {
 				auto& edge = p_edgesRow->at(j);
 				this->update_with_damping(edge->r, edge->s - first_maximum_value);
 			}
@@ -228,21 +239,21 @@ class CAffinityPropagation {
 	void update_availability() {
 		for (size_t i = 0; i < p_graph->i_matrixSize; ++i) {
 			auto& p_edgesRow = this->p_graph->pp_inputEdges[i];
-			float cumsum = 0;
+			double cumsum = 0;
 
 			for (size_t j = 0; j < p_edgesRow->size() - 1; ++j)
-				cumsum += std::max<float>(0., p_edgesRow->at(j)->r);
+				cumsum += std::max<double>(0., p_edgesRow->at(j)->r);
 
 			// self reference is at the end of vector
-			float self_responsibility = p_edgesRow->back()->r;
+			double self_responsibility = p_edgesRow->back()->r;
 
 			// update non diagonal elements
 			for (size_t j = 0; j < p_edgesRow->size() - 1; ++j)
 				this->update_with_damping(
 					p_edgesRow->at(j)->a,
-					self_responsibility + cumsum - std::max<float>(0., p_edgesRow->at(j)->r)
+					self_responsibility + cumsum - std::max<double>(0., p_edgesRow->at(j)->r)
 				);
-			
+
 			// update diagonal elements
 			this->update_with_damping(
 				p_edgesRow->back()->a,
@@ -259,14 +270,14 @@ class CAffinityPropagation {
 		for (size_t i = 0; i < p_graph->i_matrixSize; ++i) {
 			auto& p_edgesRow = this->p_graph->pp_outputEdges[i];
 
-			float max_value = -std::numeric_limits<float>::infinity();
+			double max_value = -std::numeric_limits<double>::infinity();
 			size_t max_index = std::numeric_limits<size_t>::infinity();
 
 			for (size_t j = 0; j < p_edgesRow->size(); ++j) {
 				auto& edge = p_edgesRow->at(j);
-				float tmp_val = edge->a + edge->r;
+				double tmp_val = edge->a + edge->r;
 
-				if(tmp_val > max_value){
+				if (tmp_val > max_value) {
 					max_value = tmp_val;
 					max_index = p_edgesRow->at(j)->to;
 				}
@@ -282,15 +293,16 @@ class CAffinityPropagation {
 	}
 
 public:
-	CAffinityPropagation(float damping, size_t i_maxIter) : i_damping(damping), i_maxIter(i_maxIter) {}
+	CAffinityPropagation(double damping, size_t i_maxIter, size_t i_convergenceIter = 3) :
+		i_damping(damping), i_maxIter(i_maxIter), i_convergenceIter(i_convergenceIter) {}
 
 	std::vector<size_t>& fit_predict(CGraphAP& graph) {
 		std::cout << "fitting affinity propagation" << std::endl;
 		this->p_graph = &graph;
 
 		size_t cummulated_changes = 0;
+		std::deque<size_t> convergence_queue;
 		for (size_t i = 1; i <= this->i_maxIter; ++i) {
-
 			this->update_responsibility();
 			this->update_availability();
 
@@ -298,16 +310,28 @@ public:
 			size_t unique_exemplars = this->get_unique_exemplars();
 			cummulated_changes += changes_count;
 
+			convergence_queue.push_back(changes_count);
+			if (convergence_queue.size() == this->i_convergenceIter) {
+				convergence_queue.pop_front();
+			}
+
+			if (std::accumulate(convergence_queue.begin(), convergence_queue.end(), 0) == 0) {
+				std::cout << "execution interrupted at " << i << " due to convergence" << std::endl;
+				std::cout << "total number of exemplars: " << this->get_unique_exemplars() << std::endl;
+				break;
+
+			}
+
 			if (i % 10 == 0) {
 				std::cout << "AP iteration (" << i << "/" << this->i_maxIter << "), exemplars :" << unique_exemplars << ", changes:" << cummulated_changes << std::endl;
 				cummulated_changes = 0;
 			}
 
-			if (changes_count == 0) {
-				std::cout << "execution interrupted at " << i << " due to convergence" << std::endl;
-				std::cout << "total number of exemplars: " << this->get_unique_exemplars() << std::endl;
-				break;
-			}
+			//if (changes_count == 0) {
+			//    std::cout << "execution interrupted at " << i << " due to convergence" << std::endl;
+			//    std::cout << "total number of exemplars: " << this->get_unique_exemplars() << std::endl;
+			//    break;
+			//}
 		}
 
 		return this->p_graph->exemplars;
@@ -315,16 +339,16 @@ public:
 
 	std::vector<std::vector<int>> get_top_candidates(int n_candidates) {
 		std::vector<std::vector<int>> result;
-		
+
 		for (size_t i = 0; i < p_graph->i_matrixSize; ++i) {
 			auto& p_edgesRow = this->p_graph->pp_outputEdges[i];
 
-			float* node_top = new float[n_candidates] {-std::numeric_limits<float>::infinity()};
-			int* node_ix_top = new int[n_candidates]{ -1 };
+			double* node_top = new double[n_candidates] {-std::numeric_limits<double>::infinity()};
+			int* node_ix_top = new int[n_candidates] { -1 };
 
 			for (size_t j = 0; j < p_edgesRow->size(); ++j) {
 				auto& edge = p_edgesRow->at(j);
-				float tmp_val = edge->a + edge->r;
+				double tmp_val = edge->a + edge->r;
 
 				int current_index = j;
 				for (size_t k = 0; k < n_candidates; ++k) {
@@ -354,10 +378,10 @@ int main() {
 
 	graph.read_from_file("C:/Users/Petr/JUPYTER/Gowalla/Gowalla_edges.txt", total_nodes);
 	//graph.read_from_file("C:/Users/Petr/JUPYTER/Gowalla/synth.txt", 100, true);
-	
+
 	auto&& exemplars = affinity_propagation.fit_predict(graph);
 	//auto& predictions = affinity_propagation.get_top_candidates(10);
-	
+
 
 	cache_exemplars("C:/Users/Petr/JUPYTER/Gowalla/exemplars.txt", exemplars);
 	//cache_top_results("C:/Users/Petr/JUPYTER/Gowalla/exemplars.txt", predictions);
